2227d965e6cb829685fe8baba55d42852cd0d5c6
PoliDaniel01 <daniel.poli@studenti.unitn.it>
2026-05-19 13:28:21 +0200

    fixed errors

--- code/include/ipc.h
+++ code/include/ipc.h
@@ -1,5 +1,5 @@
-#ifndef __IPC_H__
-#define __IPC_H__
+#ifndef IPC_H
+#define IPC_H
 
 #include <stdbool.h>
 #include "protocol.h"
@@ -10,7 +10,7 @@ typedef struct {
     char sender_id[16];         //String: can be a numeric ID (e.g., "0") or "EXT"
     char command[32];           //e.g., "SWITCH, "LINK, "INFO"
     int target_id;              //logical target device ID
-    char payload[MAX_MSG_LEN]   // command-specific data (e.g., "power on")
+    char payload[MAX_MSG_LEN];  // command-specific data (e.g., "power on")
 }domo_message;
 
 // IPC and FIFO function prototypes
